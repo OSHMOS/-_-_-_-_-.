a48d90d8a568a21e9e5d851f422c6e8ea82b23ae
OSHMOS <ojames9707@gmail.com>
2022-05-02 21:25:37 +0900

    에라토스테네스의 체 알고리즘을 이용하여 소수 판별 예제를 풀어보자.

--- 코딩테스트를 위한 문법/소수 판별/소수 판별.py
+++ 코딩테스트를 위한 문법/소수 판별/소수 판별.py
@@ -2,7 +2,7 @@ import math
 
 n = 1000  # 2부터 1000까지의 모든 수에 대하여 소수 판별
 # 처음엔 모든 수가 소수(True)인 것으로 초기화 (0과 1은 제외)
-array = [True i for i in range(n + 1)]
+array = [True for i in range(n + 1)]
 
 # 에라토스테네스의 체 알고리즘
 for i in range(2, int(math.sqrt(n) + 1)):  # 2부터 n의 제곱근까지의 모든 수를 확인하며
