e988c67d342457ce456d37bb677ae92b8932ac26
OSHMOS <ojames9707@gmail.com>
2022-05-02 14:03:40 +0900

    최소힙과 최대힙의 원리 공부하기

--- 코딩테스트를 위한 문법/gram.py
+++ 코딩테스트를 위한 문법/gram.py
@@ -4,8 +4,24 @@
 # h = []
 # for i in list:
 #   heapq.heappush(h, i)
+#   print(h)
 
-# print(h)
+# import heapq
+
+# def heapsort(iterable):
+#   h = []
+#   result = []
+#   # 모든 원소를 차례대로 힙에 삽입
+#   for value in iterable:
+#     heapq.heappush(h, value)
+#   # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+#   for _ in range(len(h)):
+#     # 순서와 관계없이 heappop()은 heap에서 가장 작은 원소를 반환한다.
+#     result.append(heapq.heappop(h))
+#   return result
+
+# result = heapsort([1,3,5,7,9,2,4,6,8,0])
+# print(result)
 
 import heapq
 
@@ -14,11 +30,11 @@ def heapsort(iterable):
   result = []
   # 모든 원소를 차례대로 힙에 삽입
   for value in iterable:
-    heapq.heappush(h, value)
-  print(h)
+    heapq.heappush(h, -value)
+    print(h)
   # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
   for _ in range(len(h)):
-    result.append(heapq.heappop(h))
+    result.append(-heapq.heappop(h))
   return result
 
 result = heapsort([1,3,5,7,9,2,4,6,8,0])
