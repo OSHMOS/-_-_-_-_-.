429339b43305fff5832a831ced8843a762716cbc
OSHMOS <ojames9707@gmail.com>
2022-05-02 19:21:59 +0900

    소수 판별 ver.꽤 효율적인 코드

--- 코딩테스트를 위한 문법/소수 판별.py
+++ 코딩테스트를 위한 문법/소수 판별.py
@@ -1,12 +1,14 @@
+# 끝까지 나눌 필요가 없다.
+# 제곱근까지만 나누면 된다. -> 제곱근 함수 sqrt() import math
+import math
+
+# 소수 판별 함수
+
+
 def is_prime_number(x):
-    # 2부터 (x - 1)까지의 모든 수를 확인하며
-    for i in range(2, x):
-        # x가 해당 수로 나누어 떨어진다면
+    # 2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x) + 1)):
+        # x가 해당 수로 나누어떨어진다면
         if x % i == 0:
             return False  # 소수가 아님
     return True  # 소수임
-
-
-print(is_prime_number(4))
-print(is_prime_number(7))
-# 굉장히 비효율적인 코드 -> 시간복잡도가 O(X)이기 때문이다.
