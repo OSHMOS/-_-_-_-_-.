e0567012f970db6defed1eacb4b6b734e9ef78f9
OSHMOS <ojames9707@gmail.com>
2022-05-02 19:29:47 +0900

    소수 판별 ver.가장 효율적인 알고리즘 (에라토스테네스의 체)

--- 코딩테스트를 위한 문법/소수 판별.py
+++ 코딩테스트를 위한 문법/소수 판별.py
@@ -1,14 +1,21 @@
-# 끝까지 나눌 필요가 없다.
-# 제곱근까지만 나누면 된다. -> 제곱근 함수 sqrt() import math
 import math
 
-# 소수 판별 함수
+n = 1000  # 2부터 1000까지의 모든 수에 대하여 소수 판별
+# 처음엔 모든 수가 소수(True)인 것으로 초기화 (0과 1은 제외)
+array = [True i for i in range(n + 1)]
 
+# 에라토스테네스의 체 알고리즘
+for i in range(2, int(math.sqrt(n) + 1)):  # 2부터 n의 제곱근까지의 모든 수를 확인하며
+    if array[i] == True:  # i가 소수인 경우(남은 수인 경우)
+        # j를 제외한 i의 모든 배수를 지우기
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
 
-def is_prime_number(x):
-    # 2부터 x의 제곱근까지의 모든 수를 확인하며
-    for i in range(2, int(math.sqrt(x) + 1)):
-        # x가 해당 수로 나누어떨어진다면
-        if x % i == 0:
-            return False  # 소수가 아님
-    return True  # 소수임
+# 모든 소수 출력
+for i in range(2, n + 1):
+    if array[i]:
+        print(i, end=' ')
+
+# 가장 효율적인 범위 N이 1,000,000 이내로 주어지는 경우
