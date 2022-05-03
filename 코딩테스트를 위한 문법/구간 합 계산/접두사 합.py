1c4fc80b98c976f93a7d2ab10074332f2bb02b7d
OSHMOS <ojames9707@gmail.com>
2022-05-03 21:28:24 +0900

    구간 합 빠르게 계산하기 알고리즘, 접두사 합

---
+++ 코딩테스트를 위한 문법/구간 합 계산/접두사 합.py
@@ -0,0 +1,17 @@
+# 데이터 개수 N과 전체 데이터 선언
+n = 5
+data = [10, 20, 30, 40, 50]
+
+# 접두사 합(Prefix Sum) 배열 계산
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+    sum_value += i
+    prefix_sum.append(sum_value)
+
+# 구간 합 계산(세 번째 수부터 네 번째 수까지)
+# 구간 합 계산이기 때문에 0번째 수는 없다.
+# left = 0
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left - 1])
