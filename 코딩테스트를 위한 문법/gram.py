be954f552701bf9751fd4679c7be3e8346812aab
OSHMOS <ojames9707@gmail.com>
2022-05-04 20:50:27 +0900

    파이썬 수 자료형, 실수형 궁금한 것 체크

--- 코딩테스트를 위한 문법/gram.py
+++ 코딩테스트를 위한 문법/gram.py
@@ -7,12 +7,15 @@
 # print(data)
 # print(list(data))
 
-from collections import Counter
+# from collections import Counter
 
-# 변수 = Counter(리스트)
-counter = Counter(['red', 'blue', 'red', 'green', 'blue', 'blue'])
+# # 변수 = Counter(리스트)
+# counter = Counter(['red', 'blue', 'red', 'green', 'blue', 'blue'])
 
-print(counter['blue'])
-print(counter['green'])
-# 자동으로 세서 키와 값 쌍으로 이루게 해줌
-print(dict(counter))
+# print(counter['blue'])
+# print(counter['green'])
+# # 자동으로 세서 키와 값 쌍으로 이루게 해줌
+# print(dict(counter))
+
+print(3954e-3)
+print(3954e-03)
