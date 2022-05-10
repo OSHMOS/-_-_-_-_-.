041c70ee3a05f758f11963080bc4ebed9feedd1e
OSHMOS <ojames9707@gmail.com>
2022-05-10 12:25:45 +0900

    fstring 응용

--- 코딩테스트를 위한 문법/gram.py
+++ 코딩테스트를 위한 문법/gram.py
@@ -1,21 +1,17 @@
-# from collections import deque
+# 구구단
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         print(f'{i} x {j} = {i * j}')
+print('구구단 2~9단, 열단위 출력')
+i = 1
+while i <= 9:
+    dan = 2
+    while dan <= 9:
+        print(f'{dan} x {i} = {dan * i:2}', end=" ")
+        dan += 1
+    print()
+    i += 1
 
-# data = deque([2, 3, 4])
-# data.appendleft(1)
-# data.append(5)
-
-# print(data)
-# print(list(data))
-
-# from collections import Counter
-
-# # 변수 = Counter(리스트)
-# counter = Counter(['red', 'blue', 'red', 'green', 'blue', 'blue'])
-
-# print(counter['blue'])
-# print(counter['green'])
-# # 자동으로 세서 키와 값 쌍으로 이루게 해줌
-# print(dict(counter))
-
-print(3954e-3)
-print(3954e-03)
+# 실수면 f, 정수면 d, 문자열이면 s
+# 왼쪽 정렬은 특수 기호 X, 가운데 정렬은 특수 기호 ^, 오른쪽 정렬은 특수 기호 >
+# 자릿 수 맞추기는 :(맞추고 싶은 자릿 수)
